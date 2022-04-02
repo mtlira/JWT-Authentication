698cb9d342e9c1c546f72064afc905602537b640
mtlira <matheusribeiro10@gmail.com>
2022-04-02 12:31:42 -0300

    comentarios1

--- server.js
+++ server.js
@@ -17,10 +17,23 @@ const posts = [
   }
 ]
 
+//Backend
+app.post('/login', (req, res) => {
+  // 1. Autenticacao de usuario (primeiro video)
+
+  // 2. Apos autenticacao, gerar JWT
+  const username = req.body.username
+  const user = { name: username }
+
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+  res.json({ accessToken: accessToken })
+})
+
 app.get('/posts', authenticateToken, (req, res) => {
   res.json(posts.filter(post => post.username === req.user.name))
 })
 
+// Backend (verifica se token e valido)
 function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization']
   const token = authHeader && authHeader.split(' ')[1]
